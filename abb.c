c1542bddc9efb952b44d50f718d6a857cfe7b043
liyu550 <l15163782150@163.com>
2020-10-22 19:24:21 +0800

    NEW

---
+++ abb.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: abb.c
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2020年10月22日 星期四 18时35分35秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(void)
+{
+	printf("ko no dio da.\n");
+	return 0;
+}	
